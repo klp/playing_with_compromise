4d65b4559b746b94a1ebf0019a819fbcbf7eb1b5
klp <kai.prenger@gmail.com>
2022-11-08 16:25:56 -0500

    basic POS tagging

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 const doc = nlp(text);
+const adjectives = doc.adjectives().out('array');
+const adverbs = doc.adverbs().out('array');
+const nouns = doc.nouns().out('array');
 const verbs = doc.verbs().out('array');
-console.log(verbs);
+// console.log(verbs);
+
+const outputAdjectives = adjectives.map(function(adjective){
+    return adjective;
+});
+
+const outputAdverbs = adverbs.map(function(adverb){
+    return adverb;
+});
+
+const outputNouns = nouns.map(function(noun){
+    return noun;
+});
 
 const outputVerbs = verbs.map(function(verb){
+    // TODO: strip out periods, commas, quotation marks 
     return verb;
+});
+
+outputAdjectives.forEach(function(adjective) {
+    $("#adjectives").append(adjective + " ");
+});
+
+outputAdverbs.forEach(function(adverb) {
+    $("#adverbs").append(adverb + " ");
+});
+
+outputNouns.forEach(function(noun) {
+    $("#nouns").append(noun + " ");
 });
 
 outputVerbs.forEach(function(verb) {
