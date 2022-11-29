a6ccfa7630851ce446abbad46755b77cacd6a06b
klp <kai.prenger@gmail.com>
2022-11-29 16:57:35 -0500

    wip

--- story-hour-app.js
+++ story-hour-app.js
@@ -4,5 +4,10 @@ const adverbs = doc.adverbs().out('array');
 const nouns = doc.nouns().out('array');
 const verbs = doc.verbs().out('array');
 
-
 console.log(adjectives);
+
+$("#story-of-hour").append(storyHourText);
+
+adjectives.forEach(adjective => {
+    
+});
